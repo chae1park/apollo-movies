e307e16d4db7c6df045a6fde5964495bc3e53a72
chaewon park <chaewonpark@g.skku.edu>
2021-03-21 21:37:34 +0900

    1.2

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,36 @@
-export default () => "Detail";
+// import { useQuery } from "@apollo/client";
+// import React from "react"
+// import {useParams} from "react-router-dom";
+// import Movie from "../components/Movie"
+
+
+import React from "react";
+import { useParams } from "react-router-dom";
+import { gql,useQuery } from "@apollo/client";
+
+
+const GET_MOVIES = gql` # for apollo
+    query getMovie($id: Int!){ # check the variable
+        movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+
+export default () => {
+    const {id} = useParams()
+    const {loading, data} = useQuery(GET_MOVIES, {
+        variables: {id}
+    })
+    if (loading) {
+        return "loading";
+    }
+    if (data && data.movie) {
+        return data.movie.title;
+    }
+
+    "Detail"};
